13578fcc9947e71ed68b38696459bbe4476cc849
kndlynx <96241937+kndlynx@users.noreply.github.com>
2021-12-22 22:48:13 +0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,5 +1,3 @@
-  <h2>Please refresh for updating Pie Chart</h2>
-    <div class="container">
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'pie',
